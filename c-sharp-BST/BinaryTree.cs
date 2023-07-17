f9b913bb4a6b4bcbb2a7bc65ba95b24ab74109f0
omranali2111 <aljabri1234@hotmail.com>
2023-07-17 12:39:28 +0400

    BST (LNR)

---
+++ c-sharp-BST/BinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_BST
+{
+    internal class BinaryTree
+    {
+        public Node root;
+
+
+        public Node buildTree(int[] arr, int start, int end)
+        {
+
+            if (start > end)
+            {
+                return null;
+            }
+
+
+            int mid = (start + end) / 2;
+            Node node = new Node(arr[mid]);
+
+
+            node.left = buildTree(arr, start, mid - 1);
+            node.right = buildTree(arr, mid + 1, end);
+
+
+
+            return node;
+        }
+
+        public void InOrderTraversal(Node node)
+        {
+            if (node == null)
+                return;
+
+            InOrderTraversal(node.left);
+            Console.Write(node.data + " ");
+            InOrderTraversal(node.right);
+        }
+    }
+}
---
+++ c-sharp-BST/Program.cs
@@ -0,0 +1,15 @@
+﻿using c_sharp_BST;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 7, 5, 3, 10, 9, 20, 2, 4 };
+        BinaryTree tree = new BinaryTree();
+        tree.root = tree.buildTree(arr, 0, arr.Length - 1);
+
+        Console.WriteLine("In-order traversal:");
+        tree.InOrderTraversal(tree.root);
+
+    }
+}
